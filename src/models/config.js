197ecd0e28a149070b10959ac1193e25bab70353
zhanlipeng <zhanlipeng2014@xiaochuankeji.cn>
2019-04-25 16:12:48 +0800

    v1.0

--- src/models/config.js
+++ src/models/config.js
@@ -1,17 +1,12 @@
 export default {
   namespace: 'config',
 
-  // state: {
-  //   accessKey: 'NryKyLkX_nWeFeAQU4DNbReLMNosrD4FQgBMJ9Jd',
-  //   secretKey: 'HajXVRe_UiLkY6LjgbFocCxKWXf4PLe9VTJ8w4xp',
-  //   bucket: 'zhangsan',
-  //   host:'https://images.hfuusec.cn/',
-  // },
+  
   state: {
-    accessKey: localStorage.getItem('yaccessKey'),
-    secretKey: localStorage.getItem('ysecretKey'),
-    bucket: localStorage.getItem('ybucket'),
-    host:localStorage.getItem('yhost'),
+    accessKey: localStorage.getItem('yaccessKey') || '',
+    secretKey: localStorage.getItem('ysecretKey') || '',
+    bucket: localStorage.getItem('ybucket') || '',
+    host:localStorage.getItem('yhost') || '',
   },
 
   reducers: {
